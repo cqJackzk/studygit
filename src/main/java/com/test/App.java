798e4d8975ef86f4c14a22bd4a6ff851fdd325cd
cqJackzk <jack.zk@foxmail.com>
2020-01-04 14:47:22 +0800

    init2

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println( "------------" );
         System.out.println( "Hello World!" );
+        System.out.println( "Hello Jack!" );
+        System.out.println( "Hello Jack22!" );
+        System.out.println( "Hello Jack33!" );
+        System.out.println( "Hello Jack44!" );
+
     }
 }
